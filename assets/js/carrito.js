bb30cda6c5f89bd53d441f60e8a1fc91fc81540a
mluis210393 <mluis_21@hotmail.com>
2022-02-21 10:58:07 -0600

    se pone en un script aparte

--- assets/js/carrito.js
+++ assets/js/carrito.js
@@ -1,17 +1,3 @@
-document.getElementById("shoppingCart").innerHTML = `
-    <div class="offcanvas offcanvas-end canvas-style" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
-    <div class="offcanvas-header">
-        <h5 id="offcanvasRightLabel">Carrito</h5>
-        <a class="color-close" href="javascript:void(0);" data-bs-dismiss="offcanvas">
-            <span class="icon-cross" style="font-size: 15px;"></span>
-        </a>
-    </div>
-    <div class="offcanvas-body d-flex justify-content-center">
-        Carrito de compras
-     </div> 
-    </div>
-`;
-
 const addToShopingCartButtons = document.querySelectorAll('.addToCart');
 addToShopingCartButtons.forEach(addToCartButton => {
   addToCartButton.addEventListener('click',addToCartClicked);
@@ -36,7 +22,7 @@ addItemToShoppingCart(itemTitle,itemPrice,itemImage );
 
 function addItemToShoppingCart(itemTitle,itemPrice,itemImage){
     
-    const shoppingCartRow = document.createElement('div');
+ 
     document.getElementById("shoppingCart").innerHTML = `
     <div class="offcanvas offcanvas-end canvas-style" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
     <div class="offcanvas-header">
