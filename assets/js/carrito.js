6f4ddc1f5ae86f81144be34c311f2e8cf28377e4
mluis210393 <mluis_21@hotmail.com>
2022-02-26 19:38:44 -0600

    modificaciones en el carrito

--- assets/js/carrito.js
+++ assets/js/carrito.js
@@ -6,7 +6,7 @@ addToShoppingCartButtons.forEach((addToCartButton) => {
 
   const shoppingCartItemsContainer = document.querySelector('.ShoppingC');
 
-
+  var carrito = [];
 
   function addToCartClicked(event) {
     const button = event.target;
@@ -17,10 +17,25 @@ addToShoppingCartButtons.forEach((addToCartButton) => {
   const itemPrice = item.querySelector('#priceT').textContent
   const itemImage = item.querySelector('.card-img-top').src;
   const itemAmount = item.querySelector('#name').value;
-  const itemTotal = parseInt(itemPrice) * itemAmount;
+  const itemTotal = (itemAmount>1)?parseInt(itemPrice)* itemAmount:itemPrice;
 
-  addItemToShoppingCart(itemTitle, itemImage,itemAmount,itemTotal);
+
+
+      
+      var product = {
+          Nombre: itemTitle,
+          Precio: itemPrice,
+          Url_Imagen: itemImage,
+          Cantidad: itemAmount
+        };
+        carrito.push({product})
+  
+
+sessionStorage.setItem('Carrito',JSON.stringify(carrito));
+
+addItemToShoppingCart(itemTitle, itemImage,itemAmount,itemTotal);
 }
+
 
 function addItemToShoppingCart(itemTitle,itemImage,itemAmount,itemTotal) {
     const shoppingCartRow=document.createElement('div');
@@ -29,18 +44,41 @@ function addItemToShoppingCart(itemTitle,itemImage,itemAmount,itemTotal) {
     <div class="col-sm-3">
     <img src= ${itemImage} class="img-thumbnail" alt="" style="max-height: 80px;max-width: 80px;">
      </div>
-     <div class="col-sm-3">
+     <div class="col-sm-4">
      <h5 class="card-title" id="title" name="title">${itemTitle}</h5>
      </div>
      <div class= "col-sm-3" style="padding: 15px;">
         <input type="number" id="name" class="form-control form-style form-style border border-0 border-bottom border-dark rounded-0 transparent-input jost" min="1" max="10" value=${itemAmount} name="name">
     </div>
-    <div class="col-sm-3">
-        <h4><b>$${itemTotal}</b></h4>
+    <div class="col-sm-2">
+        <h6><b>$${itemTotal}</b></h6>
     </div>  
     </div>`;
 
    shoppingCartRow.innerHTML = shoppingCartContent;
    shoppingCartItemsContainer.append(shoppingCartRow);
  
+ // updateShoppingCartTotal();
+
 }
+/*
+function updateShoppingCartTotal() {
+  let total = 0;
+  const shoppingCartTotal = document.querySelector('.shoppingCartTotal');
+
+  const shoppingCartItems = document.querySelectorAll('.shoppingCartItem');
+
+  shoppingCartItems.forEach((shoppingCartItem) => {
+    const shoppingCartItemPriceElement = shoppingCartItem.querySelector(
+      '.shoppingCartItemPrice'
+    );
+
+    }
+  }*/
+
+
+
+
+
+
+
