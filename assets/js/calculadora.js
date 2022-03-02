14d97e7bea23df2559b2b89dbd1fa1dd513c5468
mluis210393 <mluis_21@hotmail.com>
2022-03-02 16:01:12 -0600

    cambios de funcionalidades en el carrito

--- assets/js/calculadora.js
+++ assets/js/calculadora.js
@@ -1,7 +1,9 @@
 //PEDES IN TERRA AD SIDERA VISUS
 
 
+
 //funcion que recaba los datos y haces las operaciones
+
 function calcularPlanes(  genero,                 // [string] ('masculino', 'femenino')
 edad,                   // [number]
 altura,                 // [number] (en centimetros)
@@ -83,14 +85,26 @@ console.log("planes 1", r.Planes[1])
 let tdeeResult = document.getElementById('tdeeResult'); 
 let bajarResult = document.getElementById('bajarResult');
 let aumentarResult = document.getElementById('aumentarResult');
+
 let imcResult= document.getElementById('ResultIMC')
+
+
+
 
 tdeeResult.textContent = r.DEE + " Calorias"
 bajarResult.textContent = r.Planes[0] + " Calorias"
 aumentarResult.textContent = r.Planes[1] + " Calorias"
 imcResult.textContent = r.IMC
 
-}
+
+
+} //calcularPlanes()
+
+
+
+
+
+
 
 function getFormValues(){
 let edad = document.getElementById('edad').value;
@@ -156,6 +170,15 @@ let ejercitarMinutosPorDia = document.getElementById('ejercitarMinutosPorDia').v
 
 
 
+var imc = peso / ((altura / 100) * (altura / 100));
+if (imc < 18.5) {
+  console.log("IMC " + imc.toFixed(2));
+  window.addEventListener("load", milTrecientos());
+} else if (imc >= 18.5 && imc < 25) {
+  window.addEventListener("load", milSeiscientos());
+} else if (imc > 25) {
+  window.addEventListener("load", milNovecientos());
+}
 
 calcularPlanes(  genero,                 // [string] ('masculino', 'femenino')
  edad,                   // [number]
@@ -170,18 +193,87 @@ calcularPlanes(  genero,                 // [string] ('masculino', 'femenino')
 }
 
 
-document.getElementById("Calcular").addEventListener("click",Calculator);
+//Funcion que contiene e imprime el json del plan 1300
+function milTrecientos(){
+  fetch('../../assets/js/miltrescientos.json')
+  .then((elementos) => elementos.json())
+  .then(plan => {
+      let text = "";
 
+      for(let i = 0; i < (plan.length+1); i++){
+          document.getElementById("dieta").innerHTML = text;
+          (i < plan.length) ?
+          text +=   `<div class="col-lg-3" data-aos="fade-up" data-aos-duration="2000" style="padding-bottom: 25px;">
+          <div class="card card-style jost border border-0">
+              <div style="background-color: #F7F7F7;">
+                  <img src="${plan[i].img}" class="card-img-top" alt="${plan[i].name}"/>
+              </div>
+              <div class="card-body">
+                  <h4 class="card-title text-justify"><b>${plan[i].title}</b></h4>
+                  <h5>${plan[i].name}</h5>
+                  <p class="card-text text-justify">${plan[i].desc}</p>
+              </div>
+          </div>
+      </div>` : console.log("termina");
+      }//for
 
+  })
+  .catch((err) => console.log(err));
+}//milTrecientos
 
-function Calculator(){
-  
+//Funcion que contiene e imprime el json del plan 1600
+function milSeiscientos(){
+  fetch('../../assets/js/milseiscientos.json')
+  .then((elementos) => elementos.json())
+  .then(plan => {
+      let text = "";
 
-  
+      for(let i = 0; i < (plan.length+1); i++){
+          document.getElementById("dieta").innerHTML = text;
+          (i < plan.length) ?
+          text +=   `<div class="col-lg-3" data-aos="fade-up" data-aos-duration="2000" style="padding-bottom: 25px;">
+          <div class="card card-style jost border border-0">
+              <div style="background-color: #F7F7F7;">
+                  <img src="${plan[i].img}" class="card-img-top" alt="${plan[i].name}"/>
+              </div>
+              <div class="card-body">
+                  <h4 class="card-title text-justify"><b>${plan[i].title}</b></h4>
+                  <h5>${plan[i].name}</h5>
+                  <p class="card-text text-justify">${plan[i].desc}</p>
+              </div>
+          </div>
+      </div>`: console.log("termina");
+      }//for
 
+  })
+  .catch((err) => console.log(err));
+}//milSeiscientos
 
+//Funcion que contiene e imprime el json del plan 1900
+function milNovecientos(){
+  fetch('../../assets/js/milnovecientos.json')
+  .then((elementos) => elementos.json())
+  .then(plan => {
+      let text = "";
 
+      for(let i = 0; i < (plan.length+1); i++){
+          document.getElementById("dieta").innerHTML = text;
+          (i < plan.length) ?
+          text +=   `<div class="col-lg-3" data-aos="fade-up" data-aos-duration="2000" style="padding-bottom: 25px;">
+          <div class="card card-style jost border border-0">
+              <div style="background-color: #F7F7F7;">
+                  <img src="${plan[i].img}" class="card-img-top" alt="${plan[i].name}"/>
+              </div>
+              <div class="card-body">
+                  <h4 class="card-title text-justify"><b>${plan[i].title}</b></h4>
+                  <h5>${plan[i].name}</h5>
+                  <p class="card-text text-justify">${plan[i].desc}</p>
+              </div>
+          </div>
+      </div>` : console.log("termina");
+      }//for
 
+  })
+  .catch((err) => console.log(err));
+}//milNovecientos
 
-
-}
